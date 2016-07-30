db8a7855ef6599a6ce2e9c9e10569f0e3124996f
Christopher Wood <christopherwood07@gmail.com>
2016-07-30 13:39:38 -0700

    Stubbed remaining tests

--- tests/pyhex_tests.py
+++ tests/pyhex_tests.py
@@ -1,5 +1,29 @@
 from nose.tools import *
 import pyhex
 
-def test_pyhex_XX():
+def test_pyhex_hex_to_bytes():
+    pass
+
+def test_pyhex_hex_to_hex_string():
+    pass
+
+def test_pyhex_hex_string_to_bytes():
+    pass
+
+def test_pyhex_hex_string_to_hex():
+    pass
+
+def test_pyhex_bytes_to_hex():
+    pass
+
+def test_pyhex_bytes_to_hex_string():
+    pass
+
+def test_pyhex_to_bytes():
+    pass
+
+def test_pyhex_to_hex():
+    pass
+
+def test_pyhex_to_hex_string():
     pass
